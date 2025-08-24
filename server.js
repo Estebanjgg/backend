6e2f18ff8a3c56ed1e34f43298bb5dc365ab1756
Estebanjgg <esteban030991@gmail.com>
2025-08-24 20:15:48 -0300

    fix(cors): add 'x-session-id' to allowed headers for CORS

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ app.use(cors({
   },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  allowedHeaders: ['Content-Type', 'Authorization', 'x-session-id']
 }));
 
 // Middleware para parsing JSON
